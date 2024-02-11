2c17d3a32b7b6c5385ac3a25e3ab48a1eb417293
rami9237 <rami.benromdhane@etudiant-isi.utm.tn>
2024-02-11 18:33:56 +0100

    init project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>TO DO LIST workshop</h1>
+    <ul>
+      <li>Go to university</li>
+      <li>Present react workshop</li>
+      <li>Take a nap</li>
+    </ul>
+    </div>
+  );
+}
+
+export default App;
